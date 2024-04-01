d249d091df7b0a185e675b85b83af0bbab5feb88
Nihat Halici <nihat.halici@gmail.com>
2024-04-01 12:00:13 +0200

    Add files via upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,13 @@
+# accounts/forms.py
+
 from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserCreateForm, self).__init__(*args, **kwargs)
+
         for fieldname in ["username", "password1", "password2"]:
             self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update(
-                {"class": "form-control"})
+            self.fields[fieldname].widget.attrs.update({"class":"form-control"})
+
+
